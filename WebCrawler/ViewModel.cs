f8a19e9fad2f6dcafe77b6300dad2307bfc8513c
PFalkowski <piotr.falkowski.fm@gmail.com>
2016-01-31 01:26:09 +0100

    Initialize repo

---
+++ WebCrawler/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace WebCrawler
+{
+    public class ViewModel : BindableBase
+    {
+        private string _uriSelected ;
+        public string SelectedUri
+        {
+            get { return _uriSelected; }
+            set
+            {
+                if (Uri.IsWellFormedUriString(value, UriKind.RelativeOrAbsolute))
+                {
+                    _uriSelected = value;
+                }
+            }
+        }
+    }
+}
