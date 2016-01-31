7e1bd32d226de6469c3e6a6f3e9421db3bde6990
PFalkowski <piotr.falkowski.fm@gmail.com>
2016-01-31 11:16:30 +0100

    Add properties

---
+++ WebCrawler/Model.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    public class Crawler
+    {
+        public Crawler()
+        {
+        }
+
+        public async Task<string> GetFirstById(Uri from, string elementId)
+        {
+            var html = new HtmlAgilityPack.HtmlDocument();
+            using (var wc = new HttpClient())
+            {
+                var content = await wc.GetStreamAsync(from);
+                html.Load(content);
+            }
+            return html.GetElementbyId(elementId).InnerText;
+        }
+
+        //public IEnumerable<string> GetFirstById(IEnumerable<Uri> uriCollection, string elementId)
+        //{
+        //    foreach (var uri in uriCollection)
+        //    {
+        //        yield return GetFirstById(uri, elementId);
+        //    }
+        //}
+    }
+}
--- WebCrawler/ViewModel.cs
+++ WebCrawler/ViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace WebCrawler
 {
     public class ViewModel : BindableBase
     {
+        public ViewModel()
+        {
+            GoCommand = new DelegateCommand(Go);
+        }
         private string _uriSelected = "http//:";
         public string SelectedUri
         {
@@ -24,6 +30,43 @@ namespace WebCrawler
                     }
                 }
             }
+        }
+
+        private string _selectedElement;
+        public string SelectedElement
+        {
+            get { return _selectedElement; }
+            set
+            {
+                if (value != _selectedElement)
+                {
+                    _selectedElement = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private string _result;
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                if (value != _result)
+                {
+                    _result = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private Crawler _crawler = new Crawler();
+
+        public DelegateCommand GoCommand { get; private set; }
+
+        private void Go()
+        {
+            Result = _crawler.GetFirstById(new Uri(SelectedUri), SelectedElement).Result;
         }
     }
 }
